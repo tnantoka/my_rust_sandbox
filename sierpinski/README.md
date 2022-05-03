165621659a754dd9570a22013292f7002355736e
tnantoka <tnantoka@bornneet.com>
2022-05-03 21:42:12 +0900

    feat: sierpinski

---
+++ sierpinski/README.md
@@ -0,0 +1,6 @@
+https://notes.tnantoka.com/entry/2022/05/03/000003
+
+### Acknowledgements
+
+- https://github.com/PacktPublishing/Game-Development-with-Rust-and-WebAssembly
+
---
+++ sierpinski/src/lib.rs
@@ -0,0 +1,117 @@
+use rand::{thread_rng, Rng};
+use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
+
+
+// When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
+// allocator.
+//
+// If you don't want to use `wee_alloc`, you can safely delete this.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+// This is like the `main` function, except for JavaScript.
+#[wasm_bindgen(start)]
+pub fn main_js() -> Result<(), JsValue> {
+    // This provides better error messages in debug mode.
+    // It's disabled in release mode so it doesn't bloat up the file size.
+    #[cfg(debug_assertions)]
+    console_error_panic_hook::set_once();
+
+    // Your code goes here!
+    let window = web_sys::window().unwrap();
+    let document = window.document().unwrap();
+
+    let canvas = document
+        .get_element_by_id("canvas")
+        .unwrap()
+        .dyn_into::<web_sys::HtmlCanvasElement>()
+        .unwrap();
+    let context = canvas
+        .get_context("2d")
+        .unwrap()
+        .unwrap()
+        .dyn_into::<web_sys::CanvasRenderingContext2d>()
+        .unwrap();
+
+    sierpinski(
+        &context,
+        [(300.0, 0.0), (0.0, 600.0), (600.0, 600.0)],
+        random_color(),
+        5,
+    );
+
+    Ok(())
+}
+
+fn draw_triangle(
+    context: &web_sys::CanvasRenderingContext2d,
+    points: [(f64, f64); 3],
+    color: (u8, u8, u8),
+) {
+    let [top, left, right] = points;
+    let color_str = format!("rgb({}, {}, {})", color.0, color.1, color.2);
+
+    context.set_fill_style(&wasm_bindgen::JsValue::from_str(&color_str));
+    context.move_to(top.0, top.1);
+    context.begin_path();
+    context.line_to(left.0, left.1);
+    context.line_to(right.0, right.1);
+    context.line_to(top.0, top.1);
+    context.close_path();
+    context.stroke();
+    context.fill()
+}
+
+fn sierpinski(
+    context: &web_sys::CanvasRenderingContext2d,
+    points: [(f64, f64); 3],
+    color: (u8, u8, u8),
+    depth: u8,
+) {
+    draw_triangle(context, points, color);
+
+    let [top, left, right] = points;
+    let depth = depth - 1;
+
+    let next_color = random_color();
+
+    if depth > 0 {
+        let left_middle = midpoint(top, left);
+        let right_middle = midpoint(top, right);
+        let bottom_middle = midpoint(left, right);
+
+        sierpinski(
+            context,
+            [top, left_middle, right_middle],
+            next_color,
+            depth,
+        );
+        sierpinski(
+            context,
+            [left_middle, left, bottom_middle],
+            next_color,
+            depth,
+        );
+        sierpinski(
+            context,
+            [right_middle, bottom_middle, right],
+            next_color,
+            depth,
+        );
+    }
+}
+
+fn midpoint(point_1: (f64, f64), point_2: (f64, f64)) -> (f64, f64) {
+    ((point_1.0 + point_2.0) / 2.0, (point_1.1 + point_2.1) / 2.0)
+}
+
+fn random_color() -> (u8, u8, u8) {
+    let mut rng = thread_rng();
+    (
+        rng.gen_range(0..255),
+        rng.gen_range(0..255),
+        rng.gen_range(0..255),
+    )
+}
